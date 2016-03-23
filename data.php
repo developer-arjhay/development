1ccc3ab053321c8c0d33af4f1bf336cdf1e1b0b1
developer-arjhay <devarjhay@outlook.com>
2016-03-23 14:06:13 +0800

    initial release

--- data.php
+++ data.php
@@ -1,13 +1,3 @@
 <?php
-echo json_encode
-(Array(
-	Array("song_name" => "Hero",
-		"song_id" => "1990",
-		"artist_name" => "Enrique"),
-	Array("song_name" => "African Queen",
-	"song_id" => "2004",
-	"artist_name" => "Tuface")
-	Array("song_name" => "Ifunanyi",
-	"song_id" => "2012",
-	"artist_name" => "PSquare")));
+echo json_encode(Array(Array( "song_name" => "Hero", "song_id" => "1990", "artist_name" => "Enrique"), Array("song_name" => "African Queen", "song_id" => "2004", "artist_name" => "Tuface"), Array("song_name" => "Ifunanyi", "song_id" => "2012", "artist_name" => "PSquare")));
 ?>
