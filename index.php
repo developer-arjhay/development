396b6fae2f7470a95a19075d5f349b993dee8897
developer-arjhay <devarjhay@outlook.com>
2016-03-24 17:20:45 +0800

    initial release

---
+++ index.php
@@ -0,0 +1,13 @@
+{
+	"news": [
+		{
+			"image": "https://raw.githubusercontent.com/developer-arjhay/development/master/images/sampleimage.png",
+			"title": "Hello World",
+			"description": "This is the description of an item.",
+			"date": "March 24, 2016",
+			"createdBy": "Arjhay Delos Santos",
+			"sources": "https://www.google.com",
+			"avatar": "https://raw.githubusercontent.com/developer-arjhay/development/master/avatar/arjhaydelossantos.jpg"
+		}
+	]
+}
