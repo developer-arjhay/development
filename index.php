1a96577310a848a0fec6985522ceb6e9233ee190
developer-arjhay <devarjhay@outlook.com>
2016-03-24 17:53:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 		{
 			"image": "https://raw.githubusercontent.com/developer-arjhay/development/master/images/sampleimage.png",
 			"title": "Hello World",
-			"description": "This is the description of an item.",
+			"description": "<b>This is the description</b> of an item.",
 			"date": "March 24, 2016",
 			"createdBy": "Arjhay Delos Santos",
 			"sources": "https://www.google.com",
