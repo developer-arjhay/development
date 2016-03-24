f1ff35dd790295cf82ebe7884fef0553182d88df
developer-arjhay <devarjhay@outlook.com>
2016-03-24 21:01:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 {
 	"news": [
 		{
-			"image": "http://www.androidauthority.com/wp-content/uploads/2016/03/Google-Search-teaser-840x472.jpg",
-			"title": "Google Search is getting a new voice",
-			"description": "The janky, robotic voice of Google Search results that we’ve all come to know and love – at least in U.S. English – is being retired. Google is rolling out a new, smoother and less disjointed voice for spoken search results, as you can hear in the video below.\n\nThe more natural intonation and fluid delivery are much more life-like and the “gaps” between words are almost impossible to hear. It seems to be a server-side Google Text-to-Speech engine change, because the Play Store is not showing any available updates and nor are the latest APKs of Google TTS and the Google app.\n\nIf you’re not using U.S. English as your default and want to see if you have the new voice (or you just want to check out other accents), go to Settings > Language and input > Text-to-Speech output > Settings > Language to choose your system language. If necessary, go to Install voice data and choose a language pack to download.",
-			"date": "March 23, 2016",
-			"createdBy": "Arjhay Delos Santos",
-			"sources": "http://www.androidauthority.com/google-search-getting-new-voice-681707/",
-			"avatar": "https://raw.githubusercontent.com/developer-arjhay/development/master/avatar/arjhaydelossantos.jpg"
-		},
+        		"image": "https://raw.githubusercontent.com/developer-arjhay/development/master/images/sampleimage.png",
+        		"title": "Hello World",
+			"description": "Welcome to my app",
+        		"date": "March 23, 2016",
+        		"createdBy": "Arjhay Delos Santos",
+        		"sources": "null",
+        		"avatar": "https://raw.githubusercontent.com/developer-arjhay/development/master/avatar/arjhaydelossantos.jpg"
+    		},
 		{
 			"image": "http://ichef-1.bbci.co.uk/news/660/cpsprodpb/A2EE/production/_88901714_968be7d5-43c6-4514-9057-c8377067db63.jpg",
         		"title": "Google to expand Android Pay digital wallet to UK",
