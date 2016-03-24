9afd234bdf7a9265c4b8cde73d6f9f6d454a5750
developer-arjhay <devarjhay@outlook.com>
2016-03-24 19:10:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
 			"createdBy": "Arjhay Delos Santos",
 			"sources": "https://www.google.com",
 			"avatar": "https://raw.githubusercontent.com/developer-arjhay/development/master/avatar/arjhaydelossantos.jpg"
+		},
+		{
+			"image": "http://www.androidauthority.com/wp-content/uploads/2016/03/Google-Search-teaser-840x472.jpg",
+			"title": "Google Search is getting a new voice",
+			"description": "The janky, robotic voice of Google Search results that we’ve all come to know and love – at least in U.S. English – is being retired. Google is rolling out a new, smoother and less disjointed voice for spoken search results, as you can hear in the video below.",
+			"date": "March 23, 2016",
+			"createdBy": "Arjhay Delos Santos",
+			"sources": "https://www.google.com",
+			"avatar": "https://raw.githubusercontent.com/developer-arjhay/development/master/avatar/arjhaydelossantos.jpg"
 		}
 	]
 }
