47b3d9edb484749c2bcd96a1c93c624fca5e66d7
developer-arjhay <devarjhay@outlook.com>
2016-03-24 20:53:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,30 @@
 {
 	"news": [
 		{
-			"image": "https://raw.githubusercontent.com/developer-arjhay/development/master/images/sampleimage.png",
-			"title": "Hello World",
-			"description": "<b>This is the description</b> of an item.",
-			"date": "March 24, 2016",
-			"createdBy": "Arjhay Delos Santos",
-			"sources": "https://www.google.com",
-			"avatar": "https://raw.githubusercontent.com/developer-arjhay/development/master/avatar/arjhaydelossantos.jpg"
+			"image": "http://ichef-1.bbci.co.uk/news/660/cpsprodpb/A2EE/production/_88901714_968be7d5-43c6-4514-9057-c8377067db63.jpg",
+        		"title": "Google to expand Android Pay digital wallet to UK",
+			"description": "Google has said that it will extend Android Pay to the UK in the next few months.\n\nThe facility allows any device running Android 4.4 or higher and fitted with an NFC (near field communication) chip to act as a tap-and-pay substitute for credit and debit cards.\n\nSeveral lenders, including Lloyds Bank, HSBC and Nationwide have said they will support the scheme.\n\nSamsung earlier said its rival service would also come to the UK this year.\n\nThe South Korean handset maker has yet to be more specific about its British launch plans for Samsung Pay, which is restricted to its own Android smartphones.\n\nApple Pay has been available in the UK since July 2015 but is limited to the firm's iOS devices, so will not compete directly with Google's service.",
+        		"date": "March 23, 2016",
+        		"createdBy": "Arjhay Delos Santos",
+        		"sources": "http://www.bbc.com/news/technology-35874420",
+        		"avatar": "https://raw.githubusercontent.com/developer-arjhay/development/master/avatar/arjhaydelossantos.jpg"
 		},
 		{
 			"image": "http://www.androidauthority.com/wp-content/uploads/2016/03/Google-Search-teaser-840x472.jpg",
 			"title": "Google Search is getting a new voice",
-			"description": "The janky, robotic voice of Google Search results that we’ve all come to know and love – at least in U.S. English – is being retired. Google is rolling out a new, smoother and less disjointed voice for spoken search results, as you can hear in the video below.",
+			"description": "The janky, robotic voice of Google Search results that we’ve all come to know and love – at least in U.S. English – is being retired. Google is rolling out a new, smoother and less disjointed voice for spoken search results, as you can hear in the video below.\n\nThe more natural intonation and fluid delivery are much more life-like and the “gaps” between words are almost impossible to hear. It seems to be a server-side Google Text-to-Speech engine change, because the Play Store is not showing any available updates and nor are the latest APKs of Google TTS and the Google app.\n\nIf you’re not using U.S. English as your default and want to see if you have the new voice (or you just want to check out other accents), go to Settings > Language and input > Text-to-Speech output > Settings > Language to choose your system language. If necessary, go to Install voice data and choose a language pack to download.",
 			"date": "March 23, 2016",
 			"createdBy": "Arjhay Delos Santos",
-			"sources": "https://www.google.com",
+			"sources": "http://www.androidauthority.com/google-search-getting-new-voice-681707/",
+			"avatar": "https://raw.githubusercontent.com/developer-arjhay/development/master/avatar/arjhaydelossantos.jpg"
+		},
+		{
+			"image": "http://rack.3.mshcdn.com/media/ZgkyMDE2LzAzLzIzLzBiL0dvb2dsZV9OZXh1LmE1Y2FiLmpwZwpwCXRodW1iCTk1MHg1MzQjCmUJanBn/fa5e1a62/c35/Google_Nexus_9_Tablet-4.jpg",
+			"title": "Google's Android N preview has a hidden 'Night Mode' for nighttime viewing",
+			"description": "iOS users finally have the long-awaited Night Shift feature, which rolled out Monday in iOS 9.3, but Android users may not have long to wait for a similar nighttime viewing mode.\n\nA hidden feature in Google's developer preview of Android N, the next version of Android, suggests Google may be looking to add a similar nighttime viewing mode to its smartphones and tablets.\n\nThe feature is called Night Mode and, like Apple's Night Shift feature, it will change the color temperature of your display at night. The feature is tucked away in a hidden settings menu, so it's not clear if Google plans to make this an official part of its next operating system or if it's just an experiment.\n\nIf you are using the developer preview of Android N, which was released last week, you can take the feature for a spin now — if you know where to look. (While anyone with a Nexus device can opt-in to test Google's beta software, keep in mind that it's still a very early build so it's best not to test it on your primary device.)\n\nIn order to use Night Mode you first need to access a hidden settings menu called "System UI Tuner," which you can activate by holding down on the gear icon in the notifications drawer. Wait until you feel a vibration to let go. This activates the System UI Tuner menu in the device's main settings.\n\nFrom settings, head to System UI Tuner —> Color and Appearance —> Night Mode. There you can turn the feature on and off manually or opt to have it kick in automatically based on the time of day. In my testing on a Nexus 9 tablet, turning the feature on and off was a bit buggy though it worked just as well as the iOS version once it was on.\n\nWhile it's still too early to know what Google's plans for the feature are — we'll likely be hearing a lot more about Android N at Google's I/O developer conference in May — we're hoping Google plans to keep it around for the final release.",
+			"date": "March 22, 2016",
+			"createdBy": "Arjhay Delos Santos",
+			"sources": "http://mashable.com/2016/03/22/android-n-night-mode/#yThb4uzhYuq4",
 			"avatar": "https://raw.githubusercontent.com/developer-arjhay/development/master/avatar/arjhaydelossantos.jpg"
 		}
 	]
